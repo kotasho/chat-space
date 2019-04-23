59226ca54427984a65b5998fd77c4ff84f6e02ca
kotasho <sho.progress91@gmail.com>
2019-04-23 14:28:58 +0900

    addgroupsecond

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,6 +16,11 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -28,6 +33,11 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
 
 
