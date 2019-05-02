7e4e400a2b163217a80b5cf6791251744e3dfac5
kotasho <sho.progress91@gmail.com>
2019-05-02 20:13:11 +0900

    9th-adduppdate

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,14 +1,14 @@
 class Api::MessagesController < ApplicationController
   before_action :set_group
   
-  def index
-    @messages = Message.all
-    respond_to do |format|     
+   def index
+     @messages = Message.all
+     respond_to do |format|     
         format.json { @new_messages = @group.messages.where('id > ?', params[:last_id]) }  
-    end
-  end
+     end
+   end
       
-      def set_group
-        @group = Group.find(params[:group_id])
-      end
-　 end
+   def set_group
+      @group = Group.find(params[:group_id])
+   end
+end
