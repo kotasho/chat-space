f633eb5b03867269a6c4a5bb874db790e9a82e1d
kotasho <sho.progress91@gmail.com>
2019-05-02 19:14:56 +0900

    fifth-addupdate

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,6 @@
 $(function(){
   function build_messageHTML(message){
-    // var imagehtml = message.image !== null ? "" : `<img src="${message.image}class="lower-message__image">`
    
-    // var image = message.is_image_present ? `<img src='${message.image.url}'> ` : ''
-    // = image_tag message.image.url, class: 'lower-message__image' if message.image.present?
     image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
     var html = `<div class="message" data-message-id="${message.id}">
                  <div class="upper-message">
@@ -54,16 +51,13 @@ $('#item_form').on('submit', function(e){
 
   // 自動更新
   
-  // group_api_messages GET    /groups/:groupId/api/messages(.:format) api/messages#index {:format=>"json"}
-
-  // var message_id = $('.chat__contents__content:last').data('message-id');
+ 
   
 var reloadMessages = function() {
-  // どこのページで動貸すのか記述
+ 
   if (location.pathname.match(/\/groups\/\d+\/messages/)) {
 var last_message_id = $('.message').filter(":last").data('message-id')
-  // console.log(last_message_id);
-//  group_api_messages GET    /groups/:group_id/api/messages(.:format) api/messages#index {:format=>"json"}
+  
 
   $.ajax({  
     url: 'api/messages',
@@ -72,36 +66,21 @@ var last_message_id = $('.message').filter(":last").data('message-id')
     data: {last_id: last_message_id}
 })
 
-// 1 controler
-// 2
-
   .done(function(messages) {
     
     var insertHTML = '';
  
-
-  // if (xxxxxxxxx) {
-  // each xxxxxxxxxx
   
   if (messages.length !== 0){
     messages.forEach(function(message){
       if (message.id > last_message_id){
       var html = build_messageHTML(message);
       $('.messages').append(html)
-      // $(".main-contents__body").animate({scrollTop:$('.main-contents__body__list')[0].scrollHeight});
-      // ScrollToNewMessage();
+     
       }
-    })
-    
-  //   $.each(messages, function(i, message){
-  //     var html = build_messageHTML(message);
-  //   insertHTML += build_messageHTML(message);
-  //   console.log('jj')
-  //   $('.messages').append(insertHTML)
-}
-  
-  //   })
-  //  }
+    })   
+   }
+   
   })
 
   
@@ -109,11 +88,6 @@ var last_message_id = $('.message').filter(":last").data('message-id')
         alert('error');
       });
       
-    // var interval = setInterval(function(){
-    //   console.log('hhh');
-    //    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-    //    } 
-    //   })
     }
 }
     setInterval(reloadMessages, 5000);
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -5,8 +5,6 @@ class Api::MessagesController < ApplicationController
     @messages = Message.all
     respond_to do |format|     
         format.json { @new_messages = @group.messages.where('id > ?', params[:last_id]) }  
-        # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する   
-    
       end
     end
       
