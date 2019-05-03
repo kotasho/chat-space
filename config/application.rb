3f768582085786cf3a25f535458ee56a4f124b6c
kotasho <sho.progress91@gmail.com>
2019-05-03 16:48:27 +0900

    Edit-application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-    
+    end
 
    config.time_zone = 'Tokyo'
    config.active_record.default_timezone = :local
